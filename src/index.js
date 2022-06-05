321da8c9ed6d25555915e5102789abf63951b15f
Florian Hotze <florianh_dev@icloud.com>
2022-06-06 00:59:24 +0200

    (patch) Add typedefs for host classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+/**
+ * @typedef {Object} HostState Native Java openHAB State (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/types/state org.openhab.core.types.State})
+ */
+/**
+ * @typedef {Object} HostItem Native Java openHAB Item (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/items/item org.openhab.core.items.Item})
+ */
+/**
+ * @typedef {Object} HostClass Native Java Class Object (instance of java.lang.Class)
+ */
+/**
+ * @typedef {Object} HostRule Native Jave openHAB Rule (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/rule org.openhab.core.automation.Rule})
+ */
+/**
+ * @typedef {Object} HostTrigger Native Jave openHAB Trigger (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/automation/trigger org.openhab.core.automation.Trigger})
+ */
+/**
+ * @typedef {Object} HostThing Native Java openHAB Thing (instance of {@link https://www.openhab.org/javadoc/latest/org/openhab/core/thing/thing org.openhab.core.thing.Thing})
+ */
+
 module.exports = {
   get rulesx () { return require('./rules'); },
   get itemutils () { return require('./itemutils'); }
