5ee29de8bdd42a02dba938dd83234f83047ffd52
Florian Hotze <florianh_dev@icloud.com>
2022-06-06 18:23:40 +0200

    Add rainalarm
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- src/rules/alerting.js
+++ src/rules/alerting.js
@@ -12,13 +12,23 @@ const { actions, rules, items } = require('openhab');
 const { getRoofwindowOpenLevel } = require('../itemutils');
 
 /**
+ * @typedef {Object} rainalarmConfig configuration for rainalarm
+ * @memberof rulesx
+ * @property {String} rainalarmItemName name of the rainalarm Item
+ * @property {String} windspeedItemName bame of the windspeed Item-
+ * @property {String} contactGroupName name of the contact group to monitor
+ * @property {String[]} ignoreList list of contact Item names to ignore
+ * @property {String} roofwindowTag tag that all roofwindow contacts have for identification
+ * @property {Number} windspeedKlLueftung windspeed threshold for an alarm on "kleine Lüftung"
+ * @property {Number} windspeedGrLueftung windspeed threshold for an alarm on "große Lüftung"
+ */
+
+/**
  * Sends a rainalarm notification for a roowindow.
  * @private
  * @param {String} baseItemName base of the Items names, e.g. Florian_Dachfenster
  * @param {Number} windspeed current windspeed
- * @param {Object} config rainalarm configuration
- * @param {Number} config.windspeedKlLueftung windspeed threshold for an alarm on "kleine Lüftung"
- * @param {Number} config.windspeedGrLueftung windspeed threshold for an alarm on "große Lüftung"
+ * @param {rulexs.rainalarmConfig} config rainalarm configuration
  */
 const _rainalarmRoofwindow = (baseItemName, windspeed, config) => {
   const state = getRoofwindowOpenLevel(baseItemName);
@@ -38,16 +48,35 @@ const _rainalarmRoofwindow = (baseItemName, windspeed, config) => {
   }
 };
 
+/**
+ * Send a rainalarm notification for a single contact.
+ * @private
+ * @param {String} contactItem name of the contact Item.
+ */
 const _rainalarmSingleContact = (contactItem) => {
   if (contactItem.state === 'OPEN') actions.NotificationAction.sendBroadcastNotification(`Achtung! Regenalarm: ${contactItem.label} geöffnet!`);
 };
 
 /**
+ * Calls the appropiate function depending on the type of window/door.
+ * @private
+ * @param {String} itemname name of the Item
+ * @param {Number} windspeed current windspeed
+ * @param {rulesx.rainalarmConfig} config rainalarm config
+ */
+const _rainalarmContactFunction = (itemname, windspeed, config) => {
+  const tags = items.getItem(itemname).tags;
+  if (tags.includes(config.roofwindowTag)) {
+    _rainalarmRoofwindow(itemname.replace('_zu', '').replace('_klLueftung', '').replace('_grLueftung', ''), windspeed, config);
+  } else {
+    _rainalarmSingleContact(itemname);
+  }
+};
+
+/**
  * Returns the rainalarm rule.
- * @param {Object} config rainalarm configuration
- * @param {String} config.rainalarmItemName name of the rainalarm Item
- * @param {String} config.contactGroupName name of the contact group to monitor
- * @param {String[]} config.ignoreList list of contact Item names to ignore
+ * @memberof rulesx
+ * @param {rulesx.rainalarmConfig} config rainalarm configuration
  * @returns {HostRule}
  */
 const getRainalarmRule = (config) => {
@@ -58,13 +87,16 @@ const getRainalarmRule = (config) => {
 
     ],
     execute: (event) => {
+      const windspeed = parseFloat(items.getItem(config.windspeedItemName).state);
       if (event.itemName === config.rainalarmItemName) {
         console.info('Rainalarm rule is running on alarm or manual execution');
         const groupMembers = items.getItem(config.contactGroupName).members;
         for (const i in groupMembers) {
           // Check whether itemname is member of ignoreList.
-          
+          _rainalarmContactFunction(groupMembers[i], windspeed, config);
         }
+      } else {
+        _rainalarmContactFunction(event.itemName, windspeed, config);
       }
     }
   });
