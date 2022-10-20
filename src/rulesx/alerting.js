3b7440b6b3b9f3389ff09912f5316e0b11316687
Florian Hotze <florianh_dev@icloud.com>
2022-10-20 18:01:53 +0200

    [rulesx] Update duration strings for timerMgr breaking change
    Fixes #16.
    
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- src/rulesx/alerting.js
+++ src/rulesx/alerting.js
@@ -281,7 +281,7 @@ class HeatFrostalarm {
       return actions.NotificationAction.sendBroadcastNotification(`${this.config.notification.warning.title}${items.getItem(contactItem).label}${this.config.notification.warning.message}`);
     }
     // If not called on expire of timer, schedule timer.
-    const timerTime = (alarmLevel !== 4) ? time + this.config.time.addForWarning + 'm' : time + 'm';
+    const timerTime = (alarmLevel !== 4) ? 'PT' + time + this.config.time.addForWarning + 'M' : 'PT' + time + 'M';
     if (this.timerMgr.hasTimer(contactItem)) {
       console.info(`checkContact: Timer for ${contactItem} (${this.config.type}) already exists, skipping!`);
     } else {
