d8c33491eb93ab99f1019851bfdbbb0d0bf0480a
Florian Hotze <florianh_dev@icloud.com>
2023-09-18 19:57:28 +0200

    [itemutils] Major, breaking improvements to dimmer
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- src/itemutils/dimmer.js
+++ src/itemutils/dimmer.js
@@ -12,84 +12,90 @@ const { cache, items, log } = require('openhab');
 const logger = log('org.openhab.automation.js.openhab-tools.itemutils.dimmer');
 
 /**
- * Controls an Item step-by-step to a target state. Only works for Items with support for float states.
- *
- * @memberof itemutils
- * @param {string} managerID id for the dimmer manager, used as key for the cache
- * @param {string} targetItem name of the Item to control
- * @param {number} targetState target to dim to
- * @param {number} step dimming step size
- * @param {number} time time in milliseconds between each step
- * @param {boolean} [ignoreExternalChange=false] whether to break dimmer if Item receives large external change
- * @param {boolean} [overwrite=false] whether to cancel an existing dimmer and create a new one
- * @throws error when targetItem does not support float state or a parameter has wrong type
+ * Dims an Item step-by-step to a target state. Only works for Items with support for float states.
  *
  * @example
- * itemutils.dimmer('exampleManager', targetItem, targetState, step, time);
+ * // Dim the Bedroom_Light to 50% in 750 seconds (1% each 15 seconds).
+ * itemutils.dimItem('Bedroom_Light', 50.0, 1, 15 * 1000);
+ *
+ * @memberof itemutils
+ * @param {string} itemName name of the Item to control
+ * @param {number} targetState float number to dim to
+ * @param {number} step dimming step size
+ * @param {number} time time in milliseconds between each step
+ * @param {boolean} [ignoreExternalChange=false] whether to break dimmer if Item receives a large external change
+ * @param {boolean} [overwrite=false] whether to cancel an existing dimmer and create a new one
+ * @throws {TypeError} when a parameter has wrong type
+ *
  */
-const dimmer = (managerID, targetItem, targetState, step, time, ignoreExternalChange = false, overwrite = false) => {
-  const CACHE_KEY = managerID;
-  // Check Item and parameters.
-  if (typeof managerID !== 'string') throw Error('managerID must be a string.');
-  const item = items.getItem(targetItem);
+const dimItem = (itemName, targetState, step, time, ignoreExternalChange = false, overwrite = false) => {
+  // Get Item and check parameters.
+  const item = items.getItem(itemName);
   // @ts-ignore
-  if (typeof targetState !== 'number') throw Error('targetState must be a number.');
-  if (typeof step !== 'number') throw Error('step must be a number.');
-  if (typeof time !== 'number') throw Error('time must be a number.');
+  if (typeof targetState !== 'number') throw TypeError('targetState must be a number!');
+  if (typeof step !== 'number') throw TypeError('step must be a number!');
+  if (typeof time !== 'number') throw TypeError('time must be a number!');
+
+  const CACHE_KEY = 'hotzware_openhab-js-tools_dimmer-for-' + itemName;
 
   // If targetState already met, do not create a dimmer.
-  let state = parseFloat(item.state);
-  if (state === targetState) {
-    logger.debug(`${targetItem} already has targetState ${targetState}, skipping.`);
+  if (items.getItem(itemName).numericState === targetState) {
+    logger.debug(`${itemName} already has targetState ${targetState}, skipping.`);
     return;
   }
+
   // If targetState not met, create dimmer.
-  const collection = cache.private.get(CACHE_KEY, () => { return new Map(); });
-  // If dimmer for targetItem already exists, do not create new one.
-  if (collection.has(targetItem)) {
+  // If dimmer for itemName already exists, do not create new one.
+  const intervalIdFromCache = cache.shared.get(CACHE_KEY);
+  if (intervalIdFromCache !== null) {
     if (overwrite) {
-      const interval = collection.get(targetItem);
-      clearInterval(interval);
-      collection.delete(targetItem);
-      logger.info(`Dimmer ${targetItem}: already exists, overwriting.`);
+      clearInterval(intervalIdFromCache);
+      cache.shared.put(CACHE_KEY, null);
+      logger.info(`Dimmer for Item ${itemName} already exists, overwriting.`);
     } else {
-      logger.info(`Dimmer ${targetItem}: already exists, skipping.`);
+      logger.info(`Dimmer for Item ${itemName} already exists, skipping.`);
       return;
     }
   }
+
   // Initialize and create dimmer.
-  logger.info(`Dimming ${targetItem} to ${targetState}.`);
-  const interval = setInterval(() => {
-    // Function to call when the dimmer finishes or cancels.
-    const breakDimmer = (msg) => {
-      logger.info(`Dimmer ${targetItem}: Cancelled due to: ${msg}.`);
-      clearInterval(interval);
-      collection.delete(targetItem);
-    };
+  logger.info(`Dimming Item ${itemName} to ${targetState} ...`);
+  let calculatedState = items.getItem(itemName).numericState;
+  const intervalId = setInterval(() => {
+    /**
+     * Cancels the dimmer and removes the intervalId from the cache.
+     * @param {string} msg message to log
+     */
+    function breakDimmer (msg) {
+      logger.info(`Dimmer for Item ${itemName} ${msg}`);
+      clearInterval(intervalId);
+      cache.shared.put(CACHE_KEY, null);
+    }
     // Cancel when targetState is reached.
-    // Workaround for an issue where the target state is never exactly met and therefore the dimmer never ends.
-    if (Math.abs(state - targetState) <= 1) {
-      breakDimmer('reached targetState');
+    // Workaround for an issue where the target state is never exactly met because the step size is too large and therefore the dimmer never ends.
+    if (Math.abs(calculatedState - targetState) < step) {
+      item.sendCommand(targetState.toString()); // Ensure the target state is met
+      logger.trace(`Dimmer for ${itemName}: Sending command ${targetState}.`);
+      breakDimmer('reached target state.');
       return;
     }
     // Item receives large external change or state update on Item is really slow.
-    const stateRet = parseFloat(item.state);
-    if (Math.abs(stateRet - state) > 2) {
-      logger.debug(`Dimmer ${targetItem}: Difference between returned state ${stateRet} and (calculated) state ${state} is large. External item change happened or state update is slow.`);
+    const realState = items.getItem(itemName).numericState;
+    if (Math.abs(realState - calculatedState) > 2) {
+      logger.debug(`Dimmer ${itemName}: Difference between real state ${realState} and calculated state ${calculatedState} is large. External item change happened or state update is slow.`);
       if (!ignoreExternalChange) {
-        breakDimmer('external change');
+        breakDimmer('cancelled due to external change.');
         return;
       }
     }
     // Dim to target state.
-    state = (state > targetState) ? (state - step) : (state < targetState) ? (state + step) : state;
-    logger.trace(`Dimmer ${targetItem}: Sending command ${state} to ${targetItem}.`);
-    item.sendCommand(state.toString());
+    calculatedState = calculatedState > targetState ? calculatedState - step : (calculatedState < targetState ? calculatedState + step : calculatedState);
+    logger.trace(`Dimmer for ${itemName}: Sending command ${calculatedState}.`);
+    item.sendCommand(calculatedState.toString());
   }, time);
-  collection.set(targetItem, interval);
-  cache.private.put(CACHE_KEY, collection);
+  cache.shared.put(CACHE_KEY, intervalId);
 };
 
 module.exports = {
-  dimmer
+  dimItem
 };
