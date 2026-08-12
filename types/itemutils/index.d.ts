29dc07e2b73547c9440a18fe10976d5f4ba4d4b0
Florian Hotze <dev@florianhotze.com>
2026-08-12 09:21:33 +0200

    Export expire

--- types/itemutils/index.d.ts
+++ types/itemutils/index.d.ts
@@ -1,4 +1,5 @@
 declare const _exports: {
+    createExpireCountdownRule: typeof import("./expire.js").createExpireCountdownRule,
     dimItem: typeof import("./dimmer.js").dimItem;
     getGroupUtils: typeof import("./groups.js").getGroupUtils;
 };
